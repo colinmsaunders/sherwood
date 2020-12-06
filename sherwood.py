220b35b06564723d3c44ade17d5f65bb003e247b
Colin M. Saunders <colinmsaunders@gmail.com>
2020-12-05 17:37:10 -0800

    asdf

--- sherwood.py
+++ sherwood.py
@@ -9,18 +9,26 @@ PREORDER = 1
 POSTORDER = 2
 
 
-def roots(x):
+def roots(forest):
     a = []
-    for i in x.values():
+    for i in forest.values():
         if None == i[1]:
             a.append(i)
     return a
 
 
-def leafs(x):
+def leafs(forest):
     a = []
-    for i in x.values():
+    for i in forest.values():
         if 0 == len(i[3]):
+            a.append(i)
+    return a
+
+
+def parents(forest):
+    a = []
+    for i in forest.values():
+        if 0 != len(i[3]):
             a.append(i)
     return a
 
@@ -49,54 +57,56 @@ def read_forest(f):
     return x
 
 
-def dump_node(x):
-    parent = x[1]
+def dump_node(node):
+    parent = node[1]
     if None == parent:
         parent = NULL_PARENT_STR
-    s = '%s\t%s\t%s' % (x[0], parent, '\t'.join(x[2]))
+    s = '%s\t%s\t%s' % (node[0], parent, '\t'.join(node[2]))
     return s
 
 
-def walk(x, node, foo):
-    i = x[node]
-    foo(i, PREORDER)
-    for j in i[3]:
-        walk(x, j, foo)
-    foo(i, POSTORDER)
+def walk(forest, node_id, foo):
+    node = x[node_id]
+    foo(node, PREORDER)
+    for child_id in node[3]:
+        walk(forest, child_id, foo)
+    foo(node, POSTORDER)
 
 
-def stats(forest, node):
-    x = {}
-    def observe(node, order):
-        if PREORDER == order:
-            if not 'depth' in x:
-                x['depth'] = 0
-            x['depth'] += 1
-            x['max_depth'] = max(x.get('max_depth', 0), x['depth'])
-            if not 'n' in x:
-                x['n'] = 0
-            x['n'] += 1
-            if None == node[1]:
-                if not 'roots' in x:
-                    x['roots'] = 0
-                x['roots'] += 1
-            if 0 == len(node[3]):
-                if not 'leafs' in x:
-                    x['leafs'] = 0
-                x['leafs'] += 1
-        if POSTORDER == order:
-            x['depth'] -= 1
-    walk(forest, node, observe)
-    if 0 != x.get('n'):
-        x['height'] = x.get('max_depth')
-    x['parents'] = x.get('n', 0) - x.get('leafs', 0)
-    if 0 != x.get('parents', 0):
-        x['nodes_per_parent'] = x.get('n') / float(x.get('parents'))
-        x['leafs_per_parent'] = x.get('leafs', 0) / float(x.get('parents'))
+def stats(forest, node_id):
+    stats = {}
 
-    if 0 != x.get('n', 0):
-        x['node_height_density'] = x.get('n') / float(x.get('max_depth'))
-    return x
+    def walk_stats(forest, stats, node_id):
+        stat = {}
+        stat['node_id'] = node_id
+        node = forest[node_id]
+        for child_id in node[3]:
+            walk_stats(forest, stats, child_id)
+        stat['is_parent'] = 0 != len(node[3])
+        stat['is_leaf'] = 0 == len(node[3])
+        stat['children'] = len(node[3])
+        if 0 == len(node[3]):
+            stat['n'] = 1
+            stat['leafs'] = 1
+            stat['parents'] = 0
+            stat['height'] = 1
+            stat['width'] = 1
+        else:
+            stat['n'] = 1 + sum(map(lambda x: stats[x]['n'], node[3]))
+            stat['height'] = 1 + max(map(lambda x: stats[x]['height'], node[3]))
+            stat['width'] = max(map(lambda x: stats[x]['width'], node[3])) # FIXME: this is wrong
+            stat['leafs'] = sum(map(lambda x: stats[x]['leafs'], node[3]))
+            stat['parents'] = 1 + sum(map(lambda x: stats[x]['parents'], node[3]))
+
+        if 0 != len(node[3]):
+            stat['nodes_per_parent'] = stat.get('n') / float(stat.get('parents'))
+            stat['leafs_per_parent'] = stat.get('leafs', 0) / float(stat.get('parents'))
+            stat['node_height_density'] = stat.get('n') / float(stat.get('height'))
+
+        stats[node_id] = stat
+
+    walk_stats(forest, stats, node_id)
+    return stats[node_id]
 
 
 def main(argv):
@@ -111,6 +121,10 @@ def main(argv):
         x = read_forest(sys.stdin)
         for i in leafs(x):
             sys.stdout.write(dump_node(i) + '\n')
+    elif 'parents' == c:
+        x = read_forest(sys.stdin)
+        for i in parents(x):
+            sys.stdout.write(dump_node(i) + '\n')
     elif 'read' == c:
         x = read_forest(sys.stdin)
         sys.stdout.write(json.dumps(x, indent=2))
@@ -121,7 +135,7 @@ def main(argv):
         forest = read_forest(sys.stdin)
         x = stats(forest, argv[1])
         for i, j in x.items():
-            sys.stdout.write('%s\t%g\n' % (i, j))
+            sys.stdout.write('%s\t%s\n' % (i, j))
     else:
         raise Exception('i don\'t know how to \'%s\'' % c)
 
